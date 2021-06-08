9ddf26b28aa5db1a9f1c3900c1913304bf1b0821
Nolan Ngo <nolanngo@protonmail.com>
2021-06-08 19:12:38 -0400

    challenge 2 complete

--- src/data.js
+++ src/data.js
@@ -1,1 +1,11 @@
+// Challenge 1
 import data from './data.json';
+
+export default data;
+// console.log(data[0]);
+
+const allCategories = data.map(obj => obj.category);
+
+export{
+  allCategories
+}
